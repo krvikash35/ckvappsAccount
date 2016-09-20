c0466ed54053f71846989037666392363725a3bf
Vikash Kumar <krvikash35@gmail.com>
2016-09-20 19:43:49 +0530

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,5 +1,6 @@
 var log = require(__proot + "/service/shared/log/logService")
 var prop = require(__proot+'/properties');
+var error = require(__proot + "/service/error/error")
 
 module.exports = dbConfig;
 
@@ -18,7 +19,8 @@ function dbConfig(mongoose){
     //db connection close should always be logto file instead of console
     prop.db.state = 'DIRTY';
     log.setConfig({logMethod: 2});
-    log.error(new Error("DB connection closed").stack);
+    log.error( new error.DBError() );
+    // log.error(new Error("DB connection closed").stack);
     log.setConfig({logMethod: 1});
   })
 
