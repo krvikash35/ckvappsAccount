e0cfbebe1ef1dd1194a390337ad9ffd09def4f67
Vikash Kumar <krvikash35@gmail.com>
2016-09-20 19:45:43 +0530

    Update auth.service.ts

--- public/app/auth/auth.service.ts
+++ public/app/auth/auth.service.ts
@@ -2,12 +2,14 @@ import {  Injectable  } from  '@angular/core'
 import { Observable } from 'rxjs/Observable';
 import { Http, Response, URLSearchParams } from '@angular/http';
 
-import 'rxjs/add/operator/catch';
+
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 
 @Injectable()
@@ -19,22 +21,26 @@ export class AuthService {
     constructor(private http: Http) { }
 
     login(via): Observable<String> {
+        console.log("called AuthService.login with " + via)
         let params: URLSearchParams = new URLSearchParams();
         params.set('via', via);
-        return this.http.get('/login', { search: params })
-            .map(this.extractData)
-            .catch(this.handleError)
+       
+
+        return this.http.get('/login', {search: params})
+                        .map( res => res.json().data )
+                        .catch( err => Observable.throw( err.json().data ) )                   
     }
 
 
-    private extractData(res: Response) {
+    private extractData(res: Response) {        
         let body = res.json();
+        console.log( "AuthService.login on success: " +body )
         return body.data || {}
     }
 
     private handleError(error: any) {
-        console.error(error.message); // log to console instead
-        return Observable.throw(error.message);
+        console.log( "AuthService.login on error: " + error.message )       
+        return Observable.throw(error);
     }
 
 
