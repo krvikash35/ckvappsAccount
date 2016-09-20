7c0558d83434645cda41c82c1b5949334b2ba910
Vikash Kumar <krvikash35@gmail.com>
2016-09-20 19:47:26 +0530

    Update googleAuthService.js

--- service/auth/googleAuthService.js
+++ service/auth/googleAuthService.js
@@ -1,7 +1,9 @@
 'use strict'
 var prop = require(__proot + '/properties');
 var log = require(__proot + '/service/shared/log/logService');
-var request = require('request')
+var request = require('request');
+var ReqResExtracter = require(__proot + '/service/shared/log/reqResExtracter');
+
 
 module.exports = new GoogleAuthService();
 
@@ -9,11 +11,13 @@ function GoogleAuthService() {
 
 }
 
+
 GoogleAuthService.prototype.getAuthUrl = getAuthUrl;
 GoogleAuthService.prototype.getAccessToken = getAccessToken;
 
 function getAuthUrl() {
-    log.debug("called GoogleAuthService.getAuthUrl with: no param ")
+    log.debug("entered GoogleAuthService.getAuthUrl with no arg: \n" )
+
     let googleOauthStep1Url = prop.idProvider.google.oauth2Step1UrlEP + "scope=" +
         encodeURIComponent(prop.idProvider.google.scope) + "&response_type=" +
         encodeURIComponent(prop.idProvider.google.response_type) + "&client_id=" +
@@ -21,28 +25,35 @@ function getAuthUrl() {
         encodeURIComponent(prop.idProvider.google.access_type) + "&redirect_uri=" +
         encodeURIComponent(prop.oauth2.callbackurl)
 
-
-    log.debug("return GoogleAuthService.getAuthUrl with: \n" + googleOauthStep1Url)
+    log.debug("returned GoogleAuthService.getAuthUrl with : \n" + googleOauthStep1Url)
     return googleOauthStep1Url;
 }
 
-function getAccessToken(auth_code, res) {
+function getAccessToken(auth_code) {
     request.post({
             url: prop.idProvider.google.oauth2Step2UrlEP,
             form: {
                 "code": auth_code,
                 "client_id": prop.idProvider.google.client_id,
                 "client_secret": prop.idProvider.google.client_secret,
-                "redirect_uri": prop.oauth2.callbackurl
+                "redirect_uri": prop.oauth2.callbackurl,
+                "grant_type": 'authorization_code'
             }
         },
         handleAccessToken
     )
 
     function handleAccessToken(err, httpRes, body) {
-        console.log(err)
-        console.log(httpRes)
+        // log.debug("called AuthService.handleOauthCallback with : \n" + ReqResExtracter.getRequest(req));
+        console.log(err)    
         console.log(body)
+        if(body.error){
+            //error occured
+            log.error()
+            return res.send("error occured")
+        }else{
+
+        }
         return res.send("handling access")
     }
 
