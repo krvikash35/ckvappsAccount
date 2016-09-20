62bbe2f1a26d52a4b00cf792a9493763a106919b
Vikash Kumar <krvikash35@gmail.com>
2016-09-20 19:47:10 +0530

    Update authService.js

--- service/auth/authService.js
+++ service/auth/authService.js
@@ -4,6 +4,9 @@ var prop = require(__proot + '/properties')
 var log = require(__proot + '/service/shared/log/logService')
 var ReqResExtracter = require(__proot + '/service/shared/log/reqResExtracter')
 var googleAuthService = require(__proot + '/service/auth/googleAuthService');
+var error = require( __proot + '/service/error/error' );
+
+console.log(error)
 
 module.exports = new AuthService();
 
@@ -16,19 +19,37 @@ AuthService.prototype.login = login;
 AuthService.prototype.handleOauthCallback = handleOauthCallback;
 
 function login(req, res) {
-    log.debug("called AuthService.login with : \n" + ReqResExtracter.getRequest(req));
+    log.debug("entered Authservice.login with : \n" + ReqResExtracter.getRequest(req));
+    let returnData = { "data": null};
 
-    if (req.query) {
-        if (req.query.via === 'google') {
-            return res.send({
-                "data": googleAuthService.getAuthUrl()
-            });
-        }
+    if ( req.query && req.query.via ) {
+        switch( req.query.via ){
+            case "google":
+                returnData.data = googleAuthService.getAuthUrl();
+                res.status(200);
+                break;
+          
+            default:
+                let err = new error.InvalidRequestError('login method provided is not supported');
+                log.warn( err.stack );
+                returnData.data = err.resForUser;
+                res.status(400);
+        }        
+    }else{
+        let err = new error.InvalidRequestError('login method not provided');
+        log.warn( err.stack );
+        returnData.data = err.resForUser;  
+        res.status(400);      
     }
+
+    log.debug("returned Authservice.login with : \n" + returnData );
+    return res.send(returnData)
 }
 
 function handleOauthCallback(req, res) {
     log.debug("called AuthService.handleOauthCallback with : \n" + ReqResExtracter.getRequest(req));
+    let returnData = { "data": null }
+
     let referer = "google";
     if (referer === "google") {
         if (req.query) {
@@ -42,5 +63,5 @@ function handleOauthCallback(req, res) {
 
     }
 
-    // return res.send( "to be handled callback")
+    return res.send(returnData)
 }
