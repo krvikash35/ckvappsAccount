193618bddfc4a127b7874676616616a62442e2e6
Vikash Kumar <krvikash35@gmail.com>
2016-09-21 21:13:39 +0530

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,27 +1,38 @@
+var mongoose = require('mongoose');
 var log = require(__proot + "/service/shared/log/logService")
 var prop = require(__proot+'/properties');
 var error = require(__proot + "/service/error/error")
 
-module.exports = dbConfig;
+module.exports = new DBConfig();
 
-function dbConfig(mongoose){
-  log.debug( "calling dbConfig with mongoose object"  );
+function DBConfig(){
+  this.connection = null;
+}
 
-  var mongoCon = mongoose.createConnection(prop.db.uri, function(err){
+DBConfig.prototype.createConnection = createConnection;
+DBConfig.prototype.getConnection = getConnection;
+
+function createConnection(){
+  log.debug("entered DBConfig.createConnection")
+  
+  mongoCon = mongoose.createConnection( prop.db.uri, function(err){
     if(err){
-      log.error(err.stack);
-    }else {
-      log.info("connected to db" + prop.db.uri);
+      log.err( err.stack )
+    }else{
+      log.info( "opened db connection to: "+ prop.db.uri )
     }
   })
-
+  this.connection = mongoCon;
   mongoCon.on("close", function(){
-    //db connection close should always be logto file instead of console
+     //db connection close should always be logto file instead of console
     prop.db.state = 'DIRTY';
     log.setConfig({logMethod: 2});
-    log.error( new error.DBError() );
-    // log.error(new Error("DB connection closed").stack);
+    log.error( new error.DBError("DBConnection closed") );
     log.setConfig({logMethod: 1});
   })
 
+}
+
+function getConnection(){
+  return this.connection;
 }
