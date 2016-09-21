7aa7e49a709d3b39aa789be23121995f902cd048
Vikash Kumar <krvikash35@gmail.com>
2016-09-21 19:58:50 +0530

    Update properties.js

--- properties.js
+++ properties.js
@@ -13,18 +13,20 @@ module.exports = {
     },
     "idProvider": {
         "google": {
+            "name": "google",
             "client_id": "342742052739-j842arsnvvgu58j8a3a98deejetgcb8d.apps.googleusercontent.com",
-            "app_secret": "",
+            "client_secret": "FJYh1BBrDVfjRUR4ze7blNKw",
             "oauth2Step1UrlEP": "https://accounts.google.com/o/oauth2/v2/auth?",
             "scope": "email profile",
             "response_type": "code",
             "access_type": "offline",
-            "oauth2Step2UrlEP": "https://www.googleapis.com/oauth2/v4/token"
+            "oauth2Step2UrlEP": "https://www.googleapis.com/oauth2/v4/token",
+            "iss": "https://accounts.google.com"
         }
     },
     "oauth2": {
         // "callbackurl": "https://accounts.ckvapps.co.in/oauth2callback",
-        "callbackurl": "http://localhost:8082/oauth2callback"
+        "callbackurl": "http://localhost:8082/api/oauth2callback"
     }
 }
 
