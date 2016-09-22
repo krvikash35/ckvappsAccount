d43f3f59ec74cfec9e24e2dbe6a31a2d56f6c9f2
Vikash Kumar <krvikash35@gmail.com>
2016-09-22 20:22:42 +0530

    Update auth-gaurd.service.ts

--- public/app/auth/auth-gaurd.service.ts
+++ public/app/auth/auth-gaurd.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from './auth.service'
 
 @Injectable()
 export class AuthGaurd implements CanActivate{
-	canActivate(){
-		console.log('AuthGaurd#canActivate called');
-		return true;
+	constructor(private authService: AuthService, private router: Router){
+
 	}
+
+	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+		console.log('AuthGaurd#canActivate called');
+		let redirectUrl = route.queryParams['continue'] || "http://localhost:8080"
+		let userToken = route.queryParams['token']
+		
+		if(userToken && redirectUrl){
+			this.authService.saveUserToknen(userToken);
+			window.location.href=redirectUrl;
+			return false;
+		}
+		if(this.authService.isLoggedIn()){
+			window.location.href = redirectUrl
+			return false;
+		}else{
+			this.authService.redirectUrl = redirectUrl;
+			return true;
+		}
+	}
+	
 }
