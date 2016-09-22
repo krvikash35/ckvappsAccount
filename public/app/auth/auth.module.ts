87f3e0cf4ae5eb7f480f752b6310e4f5e3539a05
Vikash Kumar <krvikash35@gmail.com>
2016-09-22 13:35:11 +0530

    Update auth.module.ts

--- public/app/auth/auth.module.ts
+++ public/app/auth/auth.module.ts
@@ -3,11 +3,14 @@ import { CommonModule }   from '@angular/common';
 import { AuthComponent }  from './auth.component'
 import { authRouting } from './auth.routing';
 import { HttpModule } from '@angular/http';
+import { AuthGaurd } from './auth-gaurd.service';
+
 
 
 @NgModule({
     imports: [CommonModule, authRouting, HttpModule],
-    declarations: [AuthComponent]
+    declarations: [AuthComponent],
+    providers: [ AuthGaurd]
 })
 export class AuthModule {
 
